105f1af089e4e56870a215b81647d3b05d775849
Hamdy Mohamed <121273938+hamdymohamedak@users.noreply.github.com>
2023-09-25 20:56:54 +0300

    Add files via upload

--- src/Component/Games.js
+++ src/Component/Games.js
@@ -1,52 +1,46 @@
-import React, { useEffect } from "react";
+import React from "react";
 import LazyLoad from "react-lazy-load";
 import "./Games.css";
 import gamesData from "../games.json";
 import Footer from "./Footer";
-import format from "date-fns/format";
+
+function GameCard({
+  id,
+  title,
+  short_description,
+  platform,
+  genre,
+  developer,
+  game_url,
+  thumbnail,
+}) {
+  return (
+    <div className="card-hover">
+      <div className="card-hover__content">
+        <h3 className="card-hover__title">{title}</h3>
+        <p className="card-hover__text">{short_description}</p>
+        <p>{platform}</p>
+        <p>Genre: {genre}</p>
+        <p>Developers: {developer}</p>
+        <a href={game_url} style={{ color: "red" }} className="playNowBtn">
+          Play Now
+        </a>
+      </div>
+      <LazyLoad height={200} offset={100}>
+        <img loading="lazy" src={thumbnail} alt={title} />
+      </LazyLoad>
+    </div>
+  );
+}
 
 function Games() {
-  const RenderGameCard = React.memo(function RenderGameCard(gameProduct) {
-    return (
-      <>
-        <div className="card-hover">
-          <div className="card-hover__content">
-            <h3 className="card-hover__title">{gameProduct.title}</h3>
-            <p className="card-hover__text">{gameProduct.short_description}</p>
-            <p> {gameProduct.platform} </p>
-            <p> Genre: {gameProduct.genre} </p>
-            <p>Developers: {gameProduct.developer}</p>
-            <a href="#" className="card-hover__link">
-              <a
-                style={{ color: "red" }}
-                className="playNowBtn"
-                href={gameProduct.game_url}
-              >
-                Play Now
-              </a>
-            </a>
-          </div>
-          <LazyLoad height={200} offset={100}>
-            <img
-              loading="lazy"
-              src={gameProduct.thumbnail}
-              alt={gameProduct.id}
-            />
-          </LazyLoad>
-        </div>
-      </>
-    );
-  });
-
   return (
-    <>
-      <div className="parent">
-        {gamesData.map((gameProduct) => (
-          <RenderGameCard key={gameProduct.id} {...gameProduct} />
-        ))}
-      </div>
+    <div className="parent">
+      {gamesData.map((gameProduct) => (
+        <GameCard key={gameProduct.id} {...gameProduct} />
+      ))}
       <Footer />
-    </>
+    </div>
   );
 }
 
